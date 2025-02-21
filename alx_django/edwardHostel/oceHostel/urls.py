aa61be85b4b04f2d1f9d7a1ee0b565c212ea693d
edwardalx <eobkwaku@gmail.com>
2025-02-21 19:00:17 +0000

    week10_v1

--- alx_django/edwardHostel/oceHostel/urls.py
+++ alx_django/edwardHostel/oceHostel/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name = "index"),]
+urlpatterns = [path("", views.index, name = "index"),
+               path("list/", views.list_hostels, name = "hotel_list"),
+               ]
--- alx_django/edwardHostel/oceHostel/views.py
+++ alx_django/edwardHostel/oceHostel/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Hostels
 # Create your views here.
 
 def index(request):
     return HttpResponse("Welcome to my hostel")
+
+
+def list_hostels(request):
+    hostels = Hostels.objects.all()
+    context = {'hotels':hostels}
+    return render(request, 'hostels/list_hostels.html', context)

a9a1e674200bbe21ec3ed2ef820a7699abfc2ea2
Edward Osei-Bonsu <Edward.Osei-Bonsu@sharpgaming.onmicrosoft.com>
2025-03-04 19:49:35 +0000

    week12_v1

--- alx_django/myweek10practise/model_relation/urls.py
+++ alx_django/myweek10practise/model_relation/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import employee_list, course_list, Students
+from .views import employee_list, course_list,create_student, Students, DepartmentListCreateAPIView
 
 urlpatterns = [
     path('', view=employee_list, name='employees'),
     path('courses/', view=course_list, name='courses'),
-    path('student/', view=Students.as_view(), name='students'),
+    path('students/', view=Students.as_view(), name='students'),
+    path('api/departments', view=DepartmentListCreateAPIView .as_view(), name='departments'),
+    path('api/createstudent/', view=create_student, name='create_student'),
 ]
--- alx_django/myweek10practise/model_relation/views.py
+++ alx_django/myweek10practise/model_relation/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from .models import Employee, Course,Student, Department
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,APIView
-from .serializers import DepartmentSerializer, EmployeeSerializer
+from .serializers import DepartmentSerializer, StudentSerializer, CourseSerializer
 from rest_framework import generics
+from rest_framework import status
 
 
 
@@ -20,6 +21,17 @@ def employee_list(request):
 def course_list(request):
     courses = Course.objects.all()
     return Response({'message':'I am testing API for function type views'})
+
+
+@api_view(['POST'])
+def create_student(request):
+    serializer = StudentSerializer(data=request.data)  # Deserialize request data
+    
+    if serializer.is_valid():
+        serializer.save()  # Save the validated data to the database
+        return Response({'message': 'Student created successfully', 'data': serializer.data}, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 #API Views Class type
 
